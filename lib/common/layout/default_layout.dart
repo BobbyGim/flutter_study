df6071a4e5d681b6248e06687f97444e12181afa
BobbyGim <gimj8245@gmail.com>
2024-12-29 21:54:24 +0900

    feat: default layout appbar 추가

--- lib/common/layout/default_layout.dart
+++ lib/common/layout/default_layout.dart
@@ -2,18 +2,45 @@ import 'package:flutter/material.dart';
 
 class DefaultLayout extends StatelessWidget {
   final Widget child;
+  final Widget? bottomNavigationBar;
+
+  final String? title;
   final Color? backgroundColor;
+
   const DefaultLayout({
-    super.key,
     required this.child,
+    this.title,
     this.backgroundColor,
+    this.bottomNavigationBar,
+    super.key,
   });
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: backgroundColor ?? Colors.white,
+      appBar: renderAppBar(),
       body: child,
+      bottomNavigationBar: bottomNavigationBar,
     );
+  }
+
+  AppBar? renderAppBar() {
+    if (title == null) {
+      return null;
+    } else {
+      return AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        title: Text(
+          title!,
+          style: const TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.w700,
+          ),
+        ),
+        foregroundColor: Colors.black,
+      );
+    }
   }
 }
